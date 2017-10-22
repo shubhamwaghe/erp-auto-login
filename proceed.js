4e4b6f0e16f25dd1baff7fa3736973a85a33f33b
shubhamwaghe <shubhamwaghe@gmail.com>
2017-10-22 18:01:02 +0530

    Code cleanup and comments

--- proceed.js
+++ proceed.js
@@ -1,8 +1,12 @@
 console.log('I am logged in!');
+/* General Procedure */
 // var moduleBox = document.getElementById('moduleUL');
 // var cdcButton = moduleBox.getElementsByTagName("a")[1];
 // cdcButton.click();
 // showMenu('26','11','https://erp.iitkgp.ernet.in/TrainingPlacementSSO/TPStudent.jsp','','Y', 'CDC', 'Student', 'Application of Placement/Internship');
+
+/* Recursive function to wait for "CDC" button to appear since it's loaded 
+via AJAX afterwards, click through it afterwards */
 var scriptString = ` var moduleBox;
 function clickThrough(time) {
     if(document.getElementById("moduleUL").getElementsByTagName("a")[1] !== undefined) {
@@ -21,8 +25,3 @@ clickThrough(25); `;
 var scriptNode = document.createElement('script');
 scriptNode.textContent = scriptString;
 document.body.appendChild(scriptNode);
-// var scriptNode = document.createElement('script');
-// scriptNode.textContent = 'var cdcButton = moduleBox.getElementsByTagName("a")[1];';
-// document.body.appendChild(scriptNode);
-
-
